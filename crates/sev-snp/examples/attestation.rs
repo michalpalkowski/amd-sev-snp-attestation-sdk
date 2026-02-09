78a40bfcae95e412c738943e3ec91520d6b9e03a
Michał <michalpalkowski3@wp.pl>
2026-02-09 16:45:29 +0100

    feature: measurements

--- crates/sev-snp/examples/attestation.rs
+++ crates/sev-snp/examples/attestation.rs
@@ -7,7 +7,7 @@ fn main() {
     // Retrieve an attestation report with default options passed to the hardware device
     let (report, _) = sev_snp.get_attestation_report().unwrap();
 
-    println!("Attestation Report: {}", report);
+    println!("Attestation Report: {:?}", report);
 
     // Verify the attestation report
     sev_snp.verify_attestation_report(&report, None).unwrap();
